d152a3612a2b8403dd3d9c56e4ffc9ce10537c9d
SergeyFF3 <specialforce64044@gmail.com>
2023-03-23 12:43:59 +0300

    last commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
+    const [progress, setProgress] = useState<number>(0);
+    const [loading, setLoading] = useState<boolean>(true);
 
-    </div>
-  );
-}
+    useEffect(() => {
+        if (loading && progress <= 100) {
+            const interval = setInterval(() => {
+                setProgress((prev) => prev + 1);
+            }, 100);
+            return () => clearInterval(interval);
+
+        } else if (loading && progress > 100) {
+            setProgress(0);
+            setLoading(false);
+        }
+
+    }, [loading, progress]);
+
+    const handleButtonClick = () => {
+        setLoading((prev) => !prev);
+    };
+
+    return (
+        <div className="content">
+            <h1 className="title">Progress bar React</h1>
+            <div className="progress-container">
+                <div className="progress-bar" style={{width: `${progress}%`}}/>
+            </div>
+            <div className="button-container">
+                <button
+                    className="btn"
+                    onClick={handleButtonClick}
+                >
+                    {loading ? "Stop" : "Start"}
+                </button>
+            </div>
+        </div>
+    );
+};
 
 export default App;
